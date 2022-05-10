d5247ba50c5f50910016ff3c4854717b05ee4130
STIVVVV <stevencanfield2000@gmail.com>
2022-05-10 17:02:34 -0400

    added player movement

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid')
 let currentShooterIndex = 200
+let width = 15
 
 for (let i = 0; i < 225; i++) 
 {
@@ -29,3 +30,19 @@ function draw()
 draw()
 
 squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e)
+{
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key)
+    {
+        case 'ArrowLeft':
+            if(currentShooterIndex % width !== 0)currentShooterIndex -= 1
+            break
+        case 'ArrowRight':
+            if(currentShooterIndex % width < width - 1)currentShooterIndex += 1
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+}
+document.addEventListener('keydown', moveShooter)
